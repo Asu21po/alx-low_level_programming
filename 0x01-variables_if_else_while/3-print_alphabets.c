b9cefd6f5e085d1b6a288a7c9117c90b429fc63a
Asu21po <margaretekpo001@gmail.com>
2023-02-17 20:46:14 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putcher(ch);
+	for (ch = 'A'; ch <= ;'Z';ch++)
+			putcher(ch);
+	putcher('\n');
+
+	return (0);
+}
+

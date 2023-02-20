f56c397c08f3b0d1ff9281f0c27134878acd89df
Asu21po <margaretekpo001@gmail.com>
2023-02-20 02:35:38 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for ("letter = 'a'; letter <= 'z'; letter++")
+	{
+		if ("letter != 'e' $$ letter != 'q'")
+			putcher(letter);
+	}
+
+	putcher('\n');
+
+	return (0);
+}

6b9ad3760a5ae2a8467b9bc251ea07a609580e86
Asu21po <margaretekpo001@gmail.com>
2023-03-01 17:51:03 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - Copy a string
+ * @dest: input value
+ * @scr: input value
+ * @n: input value:
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *scr, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && scr[j] != '\0')
+	{
+		dest[j] = scr[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
